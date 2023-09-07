eb2a38909d7333545a37f93d1568c123207ddd1a
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-09-07 19:49:09 +0000

    A Building

--- ES6_classes/5-main.js
+++ ES6_classes/5-main.js
@@ -11,3 +11,13 @@ try {
 catch(err) {
     console.log(err);
 }
+
+class OfficeBuilding extends Building {
+    evacuationWarningMessage() {
+      return 'Please evacuate the office building. Fire alarm activated.';
+    }
+  }
+  
+  const office = new OfficeBuilding(5000);
+  console.log(office.sqft); // Get the sqft attribute
+  console.log(office.evacuationWarningMessage()); // Call the evacuationWarningMessage() method
