67d1d024bf86c6f5d0256dd824c9706af4f61079
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-09-12 20:09:21 +0000

    get students by location: with linting and tests

---
+++ ES6_data_manipulation/2-get_students_by_loc.test.js
@@ -0,0 +1,21 @@
+import getStudentsByLocation from "./2-get_students_by_loc.js";
+import getListStudents from "./0-get_list_students.js";
+
+test('Returns students with location by specific city', () => {
+  const students = getListStudents();
+  const city = 'San Francisco';
+  const expected = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+  const result = getStudentsByLocation(students, city);
+
+  expect(result).toEqual(expected);
+});
+
+test('Throws an error when the input is not an array', () => {
+  const input = 'Not an array';
+  const city = 'San Francisco';
+  expect(() => getStudentsByLocation(input, city)).toThrow('No students provided');
+});
+
