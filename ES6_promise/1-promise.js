d8dce19bb93cb1675732606519f0aed3350bb1f8
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-09-13 20:40:38 +0000

    Don't make a promise...if you know you can't keep it

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -15,3 +15,15 @@ When the argument is:
         ''The fake API is not working currently''
 */
 
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
