80d4a530719186478abdf671da4ed69c32f76198
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-09-13 03:06:22 +0000

    More set data structure

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,12 @@
+/*
+More set data structure
+Create a function named hasValuesFromArray that
+returns a boolean if all the elements in the array exist within the set.
+
+It accepts two arguments: a set (Set) and an array (Array).
+
+*/
+
+export default function hasValuesFromArray(set, array) {
+  return array.every((element) => set.has(element));
+}
